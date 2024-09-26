99f5f41a43708d8f25a3d70823547489b5764578
at3002 <amant9832@gmail.com>
2024-09-26 18:26:21 +0530

    Fix image format for EasyOCR

--- ocr_processing.py
+++ ocr_processing.py
@@ -1,7 +1,18 @@
 import easyocr
+import numpy as np
+from PIL import Image
 
 def ocr_image(image):
+    # Convert BytesIO object to PIL Image
+    image = Image.open(image)
+
+    # Convert the image to an array
+    image_np = np.array(image)
+
+    # Initialize EasyOCR Reader
     reader = easyocr.Reader(['en', 'hi'])  # Specify the languages you want to use
-    results = reader.readtext(image, detail=0)  # Extract text
+
+    # Read text from the image
+    results = reader.readtext(image_np, detail=0)  # Extract text
     extracted_text = ' '.join(results)  # Join the results into a single string
     return extracted_text
